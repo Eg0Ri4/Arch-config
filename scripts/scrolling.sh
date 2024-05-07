f202719f74814b3d345413774132568388731e92
Eg0Ri4 <egor9101112@gmail.com>
2024-05-07 18:54:31 +0200

    small changes

--- scripts/scrolling.sh
+++ scripts/scrolling.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 xinput set-prop "pointer:ELAN050A:01 04F3:3158 Touchpad" 338 1
-if xinput list | grep "Logitech G203 LIGHTSYNC Gaming Mouse Keyboard"; then
+if xinput list | grep "Logitech G203 LIGHTSYNC Gaming Mouse"; then
   xinput set-prop "pointer:Logitech G203 LIGHTSYNC Gaming Mouse" 338 0
 fi
