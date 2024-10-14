af1781b98e1f46ca13ce61dae229e8763667f84c
Eg0Ri4 <egor9101112@gmail.com>
2024-10-14 18:42:15 +0200

    removed some usless lines

--- scripts/scrolling.sh
+++ scripts/scrolling.sh
@@ -1,5 +1,5 @@
-#! /bin/bash
+#! /bin/ba
 xinput set-prop "pointer:ELAN050A:01 04F3:3158 Touchpad" 338 1
-if xinput list | grep "Logitech G203 LIGHTSYNC Gaming Mouse"; then
-  xinput set-prop "pointer:Logitech G203 LIGHTSYNC Gaming Mouse" 338 0
+if xinput list | grep "Mouse"; then
+  xinput set-prop  "mouse" 338 0
 fi
